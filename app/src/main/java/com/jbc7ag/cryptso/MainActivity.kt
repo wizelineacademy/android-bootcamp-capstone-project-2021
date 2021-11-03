c89a7905853708b534bb39fda7aac7dbe798fcf1
Jessica <jbc7ag@gmail.com>
2021-11-03 10:55:23 -0600

    add files and folders setting up project

---
+++ app/src/main/java/com/jbc7ag/cryptso/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jbc7ag.cryptso
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
