04f185c9ec560ec747ff7c8d02b8d201fab6a3ab
Heriberto Sanchez <heriberto_daniel@hotmail.com>
2021-11-04 09:03:38 -0600

    added data to start with app's pre-display

---
+++ app/src/main/java/com/example/bootcampproject/data/mock/MockData.kt
@@ -0,0 +1,10 @@
+package com.example.bootcampproject.data.mock
+
+import com.example.bootcampproject.domain.Currency
+
+val mockLogos = listOf(
+    Currency(id = 1L, name = "Bitcoin", imageUrl = "https://cryptologos.cc/logos/bitcoin-btc-logo.png?v=014"),
+    Currency(id = 2L, name = "Ethereum", imageUrl = "https://cryptologos.cc/logos/ethereum-eth-logo.png?v=014"),
+    Currency(id = 3L, name = "Xrp", imageUrl = "https://cryptologos.cc/logos/xrp-xrp-logo.png?v=014"),
+    Currency(id = 4L, name = "Litecoin", imageUrl = "https://cryptologos.cc/logos/litecoin-ltc-logo.png?v=014"),
+)
