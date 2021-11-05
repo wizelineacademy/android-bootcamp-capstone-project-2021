e9748b713e7dbaedda0cc7244edcc819e39aef35
Ricardo Sarabia <ricardo.sarabia-santillan@gft.com>
2021-11-04 20:00:52 -0600

    Libraries added. Networking module created.

---
+++ app/src/main/java/dev/ricsarabia/cryptochallenge/di/NetworkingModule.kt
@@ -0,0 +1,33 @@
+package dev.ricsarabia.cryptochallenge.di
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+/**
+ * Created by Ricardo Sarabia on 2021/11/04.
+ * Object to initialize and configure Retrofit's api to execute web requests.
+ */
+
+object NetworkingModule {
+    private const val BITSO_BASE_URL = "https://api.bitso.com/v3/"
+
+    fun provideRetrofitClient(): Retrofit {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .build()
+
+        return Retrofit.Builder()
+            .baseUrl(BITSO_BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
