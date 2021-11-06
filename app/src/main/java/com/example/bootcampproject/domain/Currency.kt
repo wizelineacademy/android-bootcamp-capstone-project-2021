07408c399276aa24e981f94919454eb0bb228a64
Heriberto Sanchez <heriberto_daniel@hotmail.com>
2021-11-05 19:19:11 -0600

    Added new property

--- app/src/main/java/com/example/bootcampproject/domain/Currency.kt
+++ app/src/main/java/com/example/bootcampproject/domain/Currency.kt
@@ -1,7 +1,11 @@
 package com.example.bootcampproject.domain
 
+
+import com.example.bootcampproject.data.mock.Payload
+
 data class Currency (
     val code: String,
     val name: String,
     val imageUrl: String,
+    val books: MutableList<Payload> = mutableListOf<Payload>(),
 )
