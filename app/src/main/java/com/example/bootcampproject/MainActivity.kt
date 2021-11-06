d3708741659c5d8b56bd561200c4e330a4435702
Heriberto Sanchez <heriberto_daniel@hotmail.com>
2021-11-05 19:18:15 -0600

    Added navigation functions

--- app/src/main/java/com/example/bootcampproject/MainActivity.kt
+++ app/src/main/java/com/example/bootcampproject/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.bootcampproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import com.example.bootcampproject.databinding.ActivityMainBinding
 @AndroidEntryPoint
@@ -10,5 +13,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
