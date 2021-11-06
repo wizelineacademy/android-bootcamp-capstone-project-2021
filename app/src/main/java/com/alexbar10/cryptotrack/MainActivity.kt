3c3c921d523962a0d8c11178d5c26a8ab71f9fc6
Alejandro Barranco <alexbar10@live.com.mx>
2021-11-05 20:44:32 -0600

    feature: Setup navigation controller

--- app/src/main/java/com/alexbar10/cryptotrack/MainActivity.kt
+++ app/src/main/java/com/alexbar10/cryptotrack/MainActivity.kt
@@ -2,10 +2,20 @@ package com.alexbar10.cryptotrack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.alexbar10.cryptotrack.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
