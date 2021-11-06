c8a9505ffd2439524b327d6c011617bbdba834ce
Alejandro Barranco <alexbar10@live.com.mx>
2021-11-05 19:44:15 -0600

    First commit: Setup new project

---
+++ app/src/main/java/com/alexbar10/cryptotrack/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.alexbar10.cryptotrack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
