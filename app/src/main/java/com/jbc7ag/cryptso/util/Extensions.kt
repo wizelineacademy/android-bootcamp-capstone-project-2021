1cfed300dbf7f93b921c19ea1bc3ff801b0d7ca3
Jessica <jbc7ag@gmail.com>
2021-11-20 00:02:29 -0600

    Add extension function unit test and conectivity check

--- app/src/main/java/com/jbc7ag/cryptso/util/Extensions.kt
+++ app/src/main/java/com/jbc7ag/cryptso/util/Extensions.kt
@@ -1,10 +1,7 @@
 package com.jbc7ag.cryptso.util
 
-import android.content.Context
-import android.util.Log
-import com.jbc7ag.cryptso.R
+
 import com.jbc7ag.cryptso.data.model.*
-import retrofit2.Response
 import java.text.DecimalFormat
 import java.text.NumberFormat
 
@@ -34,14 +31,6 @@ fun String.formatCurrency(): String {
     return formatter.format(this.toFloat())
 }
 
-fun String.formatMaxPrice(code: String, context: Context): String {
-    if (code.isEmpty()) return EMPTY_STRING
-
-    val convertCode = code.substring(code.indexOf(DELIMITER) + 1, code.length).uppercase()
-    return "${context.getString(R.string.maximum_price)} ${this.formatCurrency()} ${convertCode.uppercase()}"
-}
-
-
 fun List<Book>.getFilterList(item: String?): List<Filter> {
     val filters = mutableListOf<Filter>()
     this.map {
@@ -49,19 +38,3 @@ fun List<Book>.getFilterList(item: String?): List<Filter> {
     }
     return filters.distinctBy{ it.name }
 }
-
-fun Response<Orders>?.getOrderError(): String {
-    if (this?.body() == null) return EMPTY_STRING
-    return "${this.body()?.error?.code}: ${this.body()?.error?.message}"
-}
-
-fun Response<Ticker>?.getTickerError(): String {
-    if (this?.body() == null) return EMPTY_STRING
-    return "${this.body()?.error?.code}: ${this.body()?.error?.message}"
-}
-
-fun Response<AvailableBooks>?.getBooksError(): String {
-    if (this?.body() == null) return EMPTY_STRING
-    return "${this.body()?.error?.code}: ${this.body()?.error?.message}"
-}
-
---
+++ app/src/main/java/com/jbc7ag/cryptso/util/isInternetAvailable.kt
@@ -0,0 +1,32 @@
+package com.jbc7ag.cryptso.util
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
+
+fun isInternetAvailable(context: Context): Boolean {
+
+    var result = false
+    val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+        cm.let{
+            it.getNetworkCapabilities(cm.activeNetwork)?.apply {
+                result = when{
+                    hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                    hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                    else -> false
+                }
+            }
+
+        }
+        return result
+    }
+    else{
+        cm.activeNetworkInfo.also {
+            return it != null && it.isConnected
+        }
+    }
+}
