cc3e9bf9e816afbfcc5b87a87dc830be6a6b2a35
Alejandro Barranco <alexbar10@live.com.mx>
2021-11-22 17:23:25 -0600

    Feature: Add HttpLoggingInterceptor for all network events

--- app/src/main/java/com/alexbar10/cryptotrack/di/NetworkingModule.kt
+++ app/src/main/java/com/alexbar10/cryptotrack/di/NetworkingModule.kt
@@ -8,6 +8,9 @@ import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
+import okhttp3.OkHttpClient
+
+import okhttp3.logging.HttpLoggingInterceptor
 
 private const val API_BASE_URL = "https://api.bitso.com/v3/"
 
@@ -18,9 +21,14 @@ object NetworkingModule {
     @Provides
     @Singleton
     fun provideRetrofitClient(): Retrofit {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val okHttpClient: OkHttpClient = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
         return Retrofit.Builder()
             .baseUrl(API_BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
             .build()
     }
 
