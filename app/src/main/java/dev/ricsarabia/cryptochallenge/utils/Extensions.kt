a85356e720ef4dad2e8f765039e23ebdcba0dc2e
Ricardo Sarabia <ricardo.sarabia-santillan@gft.com>
2021-11-24 03:49:37 -0600

    String format extensions updated.

--- app/src/main/java/dev/ricsarabia/cryptochallenge/utils/Extensions.kt
+++ app/src/main/java/dev/ricsarabia/cryptochallenge/utils/Extensions.kt
@@ -2,18 +2,30 @@ package dev.ricsarabia.cryptochallenge.utils
 
 import java.lang.Exception
 import java.text.DecimalFormat
+import java.time.LocalDate
+import java.time.OffsetTime
+import java.time.format.DateTimeFormatter
 
 /**
  * Created by Ricardo Sarabia on 2021/11/18.
  */
 fun String.asDecimalAmount(): String = try {
-    DecimalFormat("#,##0.00").format(this.toDouble())
+    DecimalFormat("#,##0.00######").format(this.toDouble())
 } catch (e: Exception) {
     "0.00"
 }
 
 fun String.asDecimalPrice(): String = try {
-    DecimalFormat("$ #,##0.00").format(this.toDouble())
+    DecimalFormat("$ #,##0.00######").format(this.toDouble())
 } catch (e: Exception) {
     "$ 0.00"
+}
+
+fun String.asLocalDate(): String = try {
+    val bitsoFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX")
+    val date = LocalDate.parse(this, bitsoFormatter)
+    val localFormatter = DateTimeFormatter.ofPattern("d MMM yyyy, HH:mm:ss")
+    date.atTime(OffsetTime.now()).format(localFormatter)
+} catch (e: Exception) {
+    "never"
 }
