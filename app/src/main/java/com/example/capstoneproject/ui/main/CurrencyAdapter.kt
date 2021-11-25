c5c68cd6ab055587ae2ac1cec8cd7d8067efa081
Cristina Silva <silva.hernandez2107@gmail.com>
2021-11-24 19:05:21 -0600

    Use retrofit version for converter

--- app/src/main/java/com/example/capstoneproject/ui/main/CurrencyAdapter.kt
+++ app/src/main/java/com/example/capstoneproject/ui/main/CurrencyAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.capstoneproject.ui.main
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
