31886f9b55206ec482f3ebd126e900ee9e8f1f3a
Jessica <jbc7ag@gmail.com>
2021-11-26 16:35:13 -0600

    update config and fix tests

--- app/src/androidTest/java/com/jbc7ag/cryptso/Utils.kt
+++ app/src/androidTest/java/com/jbc7ag/cryptso/Utils.kt
@@ -1,5 +1,6 @@
 package com.jbc7ag.cryptso
 
+import android.app.Activity
 import android.content.ComponentName
 import android.content.Intent
 import android.os.Bundle
@@ -9,6 +10,10 @@ import androidx.fragment.app.testing.FragmentScenario
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.core.internal.deps.dagger.internal.Preconditions
+import org.hamcrest.BaseMatcher
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+
 
 inline fun <reified T : Fragment> launchFragmentInHiltContainer(
     fragmentArgs: Bundle? = null,
@@ -30,9 +35,26 @@ inline fun <reified T : Fragment> launchFragmentInHiltContainer(
         fragment.arguments = fragmentArgs
         activity.supportFragmentManager
             .beginTransaction()
-            .add(android.R.id.content, fragment, "")
+            .add(android.R.id.content, fragment, "new_fragment")
             .commitNow()
 
         fragment.action()
+    }
+}
+
+fun <T> first(matcher: Matcher<T>): Matcher<T>? {
+    return object : BaseMatcher<T>() {
+        var isFirst = true
+        override fun matches(item: Any): Boolean {
+            if (isFirst && matcher.matches(item)) {
+                isFirst = false
+                return true
+            }
+            return false
+        }
+
+        override fun describeTo(description: Description) {
+            description.appendText("should return first matching item")
+        }
     }
 }
--- app/src/androidTest/java/com/jbc7ag/cryptso/ui/currencieslist/CurrenciesFragmentTest.kt
+++ app/src/androidTest/java/com/jbc7ag/cryptso/ui/currencieslist/CurrenciesFragmentTest.kt
@@ -1,66 +1,31 @@
 package com.jbc7ag.cryptso.ui.currencieslist
 
-import android.os.Bundle
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import androidx.recyclerview.widget.RecyclerView
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItem
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.filters.MediumTest
 import com.jbc7ag.cryptso.MainActivity
-import com.jbc7ag.cryptso.launchFragmentInHiltContainer
 import com.jbc7ag.cryptso.R
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.hamcrest.CoreMatchers.allOf
 import org.junit.Rule
 import org.junit.Test
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.verify
 
 @MediumTest
 @ExperimentalCoroutinesApi
 class CurrenciesFragmentTest {
 
     @get:Rule
-    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+    var activityRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
     fun screen_is_shown() = runBlockingTest {
-
         onView(withText("Cryptso")).check(matches(isDisplayed()))
         onView(allOf(withId(R.id.filter_list), isDisplayed()))
         onView(allOf(withId(R.id.currencies_list), isDisplayed()))
-    }
-
-    @Test
-    fun click_currency_navigateToDetailsFragment() {
-
-        val navController = mock(NavController::class.java)
-
-        launchFragmentInHiltContainer<CurrenciesFragment>(
-            Bundle(),
-            R.style.Theme_AppCompat_DayNight_NoActionBar
-        ) {
-            Navigation.setViewNavController(view!!, navController)
-        }
-
-        onView(allOf(withId(R.id.currencies_list), isDisplayed()))
-           /* .perform(
-                actionOnItem<RecyclerView.ViewHolder>(
-                    hasDescendant(withText("Bitcoin")), ViewActions.click()
-                )
-            )
-
-        verify(navController).navigate(
-            CurrenciesFragmentDirections.actionCurrenciesFragmentToCurrencyDetailFragment("btc_mxn")
-        )*/
     }
 }
